bce7636c6a1d71400e5fc1b848be2bc4fd2cfc90
ashutosh rajput <rajputashutosh81@gmail.com>
2024-11-30 08:09:19 +0530

    testing pipeline test 7

--- src/main/java/AshutoshRajput/Softnerve/AppConfig/SwaggerConfig.java
+++ src/main/java/AshutoshRajput/Softnerve/AppConfig/SwaggerConfig.java
@@ -58,8 +58,8 @@ public class SwaggerConfig {
                                 "**Database:** `MongoDB`\n")
                         .version("1.0")
                         .contact(new Contact()
-                                .name("Ashutosh Rajput")
-                                .email("rajputashutosh81@gmail.com"))
+                                .name("Diksha sharma")
+                                .email("deiskasharma81@gmail.com"))
                 )
 
                 .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
