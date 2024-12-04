9c457ac95543ba9e4ee3cbae8cff0f27fb938630
ashutosh rajput <84820887+Ashutosh-rajput@users.noreply.github.com>
2024-12-04 15:51:37 +0530

    Update SwaggerConfig.java

--- src/main/java/AshutoshRajput/Softnerve/AppConfig/SwaggerConfig.java
+++ src/main/java/AshutoshRajput/Softnerve/AppConfig/SwaggerConfig.java
@@ -58,7 +58,7 @@ public class SwaggerConfig {
                                 "**Database:** `MongoDB`\n")
                         .version("1.0")
                         .contact(new Contact()
-                                .name("Admin")
+                                .name("Ashutosh rajput")
                                 .email("admin@gmail.com"))
                 )
 
