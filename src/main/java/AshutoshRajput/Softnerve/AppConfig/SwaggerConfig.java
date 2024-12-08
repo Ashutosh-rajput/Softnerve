add9cc8407f0df331b669992001060e97adcb3bb
ashutosh rajput <rajputashutosh81@gmail.com>
2024-12-09 02:35:39 +0530

    testing pipeline test 10

--- src/main/java/AshutoshRajput/Softnerve/AppConfig/SwaggerConfig.java
+++ src/main/java/AshutoshRajput/Softnerve/AppConfig/SwaggerConfig.java
@@ -1,55 +1,34 @@
 package AshutoshRajput.Softnerve.AppConfig;
 
-
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Contact;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
-import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
 public class SwaggerConfig {
 
     @Bean
-    public GroupedOpenApi studentApi() {
-        return GroupedOpenApi.builder()
-                .group("Student API")
-                .pathsToMatch("**")
-                .build();
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.softnerve.Patient_MicroService.controller"))
+                .build()
+                .apiInfo(apiInfo()); // Attach metadata here
     }
 
-
-    @Bean
-    public OpenAPI customOpenAPI() {
-        return new OpenAPI()
-                .info(new Info()
-                        .title("Student Management Application")
-                        .description("This project is designed student to manage information and offers functions for creating, updating, deleting, and retrieving student records. Additionally, it includes a login API for authentication using JWT tokens. The `create student` API is an open endpoint. To use it, you will need to first log in, then copy the JWT token and paste it into the header for authentication.\n\n" +
-                                "### API Summary\n" +
-                                "   - **POST:** `Create Student`\n" +
-                                "   - **GET:** `Get Student by ID`\n" +
-                                "   - **PUT:** `Update Student`\n" +
-                                "   - **DELETE:** `Delete Student`\n" +
-                                "   - **GET:** `Get List of Students`\n" +
-                                "   - **POST:** `Login`\n" +
-                                "**Database:** `MongoDB`\n")
-                        .version("1.0")
-                        .contact(new Contact()
-                                .name("Admin")
-                                .email("admin@gmail.com"))
-                )
-
-                .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
-                .components(new io.swagger.v3.oas.models.Components()
-                        .addSecuritySchemes("Bearer Authentication", new SecurityScheme()
-                                .type(SecurityScheme.Type.HTTP)
-                                .scheme("bearer")
-                                .bearerFormat("JWT")
-                                .in(SecurityScheme.In.HEADER)
-                                .name("Authorization")));
-
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Student Management Application")
+                .description("This project is designed to manage student information and offers functions for creating, updating, deleting, and retrieving student records. " +
+                        "Additionally, it includes a login API for authentication using JWT tokens. " +
+                        "The create student API is an open endpoint. To use it, you will need to first log in, then copy the JWT token and paste it into the header for authentication.")
+                .version("1.0")
+                .contact(new Contact("Ashutosh Rajput", null, "rajputashuotsh81@gmail.com"))
+                .build();
     }
 }
--- src/main/java/AshutoshRajput/Softnerve/Controller/StudentController.java
+++ src/main/java/AshutoshRajput/Softnerve/Controller/StudentController.java
@@ -4,6 +4,8 @@ import AshutoshRajput.Softnerve.DTO.StudentDTO;
 import AshutoshRajput.Softnerve.Entity.Student;
 import AshutoshRajput.Softnerve.Service.ServiceImpl.MongoDbAutoSequenceService;
 import AshutoshRajput.Softnerve.Service.ServiceImpl.StudentServiceImpl;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -29,6 +31,11 @@ public class StudentController {
     }
 
     @PostMapping("/create")
+    @Operation(
+            summary = "Create a new student",
+            description = "This endpoint allows creating a new student without requiring authentication."
+    )
+
     public StudentDTO createStudent(@RequestBody @Valid StudentDTO studentDTO) {
         studentDTO.setId(sequenceService.getsequencenumber(Student.SEQUENCE_NAME));
 
@@ -36,22 +43,26 @@ public class StudentController {
     }
 
     @GetMapping("/get/{id}")
+    @SecurityRequirement(name = "BearerAuth")
     public ResponseEntity<StudentDTO> getStudentById(@PathVariable Long id) {
         return ResponseEntity.ok(studentService.getStudentByid(id));
     }
 
     @PutMapping("/update/{id}")
+    @SecurityRequirement(name = "BearerAuth")
     public ResponseEntity<StudentDTO> updateStudent(@RequestBody @Valid StudentDTO studentDTO, @PathVariable Long id) {
         return ResponseEntity.ok(studentService.upadateStudent(studentDTO, id));
     }
 
     @DeleteMapping("/delete/{id}")
+    @SecurityRequirement(name = "BearerAuth")
     public ResponseEntity<String> deleteStudentById(@PathVariable Long id) {
         studentService.deleteStudent(id);
         return ResponseEntity.ok("Student deleted successfully with ID: " + id);
     }
 
     @GetMapping("/students/{page}/{size}")
+    @SecurityRequirement(name = "BearerAuth")
     public ResponseEntity<Page<StudentDTO>> getListOfStudents(@PathVariable int page, @PathVariable int size) {
         return ResponseEntity.ok(studentService.ListOfStudent(page, size));
     }
